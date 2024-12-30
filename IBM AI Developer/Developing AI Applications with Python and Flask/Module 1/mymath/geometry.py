4425878cdbc87c19cf67722fef501a1736ccb378
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2024-12-30 05:48:09 +0000

    making packages

---
+++ IBM AI Developer/Developing AI Applications with Python and Flask/Module 1/mymath/geometry.py
@@ -0,0 +1,25 @@
+"""This module contains geometry functions
+"""
+
+def area_of_rectangle(length, width):
+    """Calculate the area of a rectangle
+
+    Args:
+        length: The length of the rectangle
+        width: The width of the rectangle
+
+    Returns:
+        The area of the rectangle
+    """
+    return length * width
+
+def area_of_circle(radius):
+    """Calculate the area of a circle
+
+    Args:
+        radius: The radius of the circle
+
+    Returns:
+        The area of the circle
+    """
+    return 3.14159 * (radius ** 2)
