c52ec984e2b6f117e44a03cd86f76089555c905b
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2024-12-31 01:12:58 +0000

    "backend" implementation

---
+++ IBM AI Developer/Developing AI Applications with Python and Flask/Module 2/lab/hjbsk-build_deploy_app_flask/Maths/mathematics.py
@@ -0,0 +1,36 @@
+def summation(a, b):
+    """This function returns the sum of two numbers
+
+    Args:
+        a (int): First number
+        b (int): Second number
+
+    Returns:
+        int: Sum of a and b
+    """
+    return a + b
+
+def subtraction(a, b):
+    """This function returns the difference between two numbers
+
+    Args:
+        a (int): First number
+        b (int): Second number
+
+    Returns:
+        int: Difference of a and b
+    """
+    return a - b
+
+def multiplication(a, b):
+    """This function returns the product of two numbers
+
+    Args:
+        a (int): First number
+        b (int): Second number
+
+    Returns:
+        int: Product of a and b
+    """
+    return a * b
+
